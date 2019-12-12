77dd4416845fe5813521feed559c6db680872342
Tiago Lisboa Alves <tiago.alves@serpro.gov.br>
2019-12-12 14:25:01 -0300

    Arquivos de build

---
+++ component---src-pages-about-js-42577452a9eebcc8d141.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3XHS":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a);t.default=function(){return l.a.createElement("main",null,l.a.createElement("h1",null,"About the Author"),l.a.createElement("p",null,"Welcome to my Gatsby site."))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-42577452a9eebcc8d141.js.map
